1018d640da223200f4b8481de768d5d8afa757e7
vasu412 <vasusingla44@gmail.com>
2024-04-27 15:12:40 +0530

    a bit change

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <>
       <div>
         <h1>Learn Web Development</h1>
-        <p>Welcome to the MDN learning area. This set of articles aims to provide complete beginners to web development with fundamental skills for coding websites. <br /><br />The aim is not to take you from "beginner" to "expert" but to take you from "beginner" to "comfortable." From there, you should be able to start making your way, learning from the rest of MDN, and other intermediate to advanced resources that assume a lot of previous knowledge. <br /><br />
+        <p>Welcome to the MDN learning area. This set of articles aims to provide complete beginners to web development with fundamental skills for coding websites. <br /><br />The aim is not to take you from "beginner" to "expert" but to take you from "beginner" to "comfortable." From there, you should be able to start making your way, learning from <a href="">the rest of MDN</a>, and other intermediate to advanced resources that assume a lot of previous knowledge. <br /><br />
 
         If you are a complete beginner, web development can be challenging — we will hold your hand and provide enough detail for you to feel comfortable and learn the topics properly. You should feel at home whether you are a student learning web development (on your own or as part of a class), a teacher looking for class materials, a hobbyist, or someone who just wants to understand more about how web technologies work.</p>
       </div>
