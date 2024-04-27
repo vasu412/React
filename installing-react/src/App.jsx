2b8c41612d494406d813ac1a7b725510d9c6ead5
vasu412 <vasusingla44@gmail.com>
2024-04-27 15:09:31 +0530

    pending commit

--- installing-react/src/App.jsx
+++ installing-react/src/App.jsx
@@ -4,30 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Topics Covered</h1>
+    <p>
+    The following is a list of all the topics we cover in the MDN learning area.
+    <br />
+    <br />
+    <a href="">Getting started with the web</a><br /><br />
+    <span class='span'></span>Provides a practical introduction to web development for complete beginners.
+    <br />
+    <br />
+    <a href="">HTML — Structuring the web</a>
+    <br />
+    <br />
+    <span className='span'></span>
+    HTML is the language that we use to structure the different parts of our content and define what their meaning or purpose is. This topic teaches HTML in detail.
+    <br />
+    <br />
+    <a href="">CSS — Styling the web</a>
+    <br />
+    <br />
+    <span className="span"></span>
+    CSS is the language that we use to control our web content's style and layout, as well as adding behavior like animation. This topic provides comprehensive coverage of CSS.
+    </p>
     </>
   )
 }
